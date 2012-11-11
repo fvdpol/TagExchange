7d6033a95cc869548c38d57319c783b25446ac34
Frank van de Pol <fvdpol@gmail.com>
2012-11-11 20:07:11 +0100

    update TagExchange lib with support for Serial in/out

--- TagExchange.cpp
+++ TagExchange.cpp
@@ -43,6 +43,60 @@ void TagExchange::initTagTxBuffer(void)
 }
 
 
+void TagExchange::processInboundPacket(Packet *packet)	// handle inbound message
+{
+	//Serial.println(F("TagExchange::processInboundPacket()"));
+
+	switch(packet->msg_id) {
+		case msg_tag: 
+			// process tag message
+			for (byte i = 0; i < packet->payload.TagData.tagcount; ++i) {
+			  
+			   int  tagid = packet->payload.TagData.data[i].tagid & 0x3fff;              // lower 14 bits determine the tagid;
+			   TagType tagtype = (TagType)((packet->payload.TagData.data[i].tagid & 0xc000) >> 14);   // upper 2 bits determine the data type
+											   
+			   switch(tagtype) {              
+				 case tagtype_float:
+					if (_floatHandler_callback != NULL) {
+						(*_floatHandler_callback)(tagid, packet->payload.TagData.timestamp, packet->payload.TagData.data[i].value.float_value);
+					}                   
+				   break;
+					 
+				 case tagtype_long:
+					if (_longHandler_callback != NULL) {
+						(*_longHandler_callback)(tagid, packet->payload.TagData.timestamp, packet->payload.TagData.data[i].value.long_value);
+					}                 
+				   break;
+				   
+				 case tagtype_text:
+					if (_textHandler_callback != NULL) {
+					(*_textHandler_callback)(tagid, packet->payload.TagData.timestamp, packet->payload.TagData.data[i].value.text_value);
+					}
+				   
+			   }               
+			}
+		break;
+		
+		
+		case msg_tagrequest:
+			Serial.println(F("Unhandled message: msg_tagrequest"));
+			break;
+			
+		case msg_taggroup:
+			Serial.println(F("Unhandled message: msg_taggroup"));
+			break;
+
+		case msg_taggrouprequest:
+			Serial.println(F("Unhandled message: msg_taggrouprequest"));
+			break;
+			
+		default:
+			Serial.println(F("Unhandled message: user defined"));
+	}
+
+}
+
+
 
 void TagExchange::publishFloat(int tagid, float value)
 {
@@ -55,8 +109,9 @@ void TagExchange::publishFloat(int tagid, unsigned long timestamp, float value)
 	if (timestamp != _tagtxpacket.timestamp || _tagtxpacket.tagcount >= MAX_TAGS_PER_RF12_MSG) {
 		publishNow(true);
 	}
-	
+
 	if (_tagtxpacket.tagcount == 0) {
+		// initialise timestamp	
 		_tagtxpacket.timestamp = timestamp;
 		
 		// set timeout value for the transmission delay
@@ -86,6 +141,7 @@ void TagExchange::publishLong(int tagid, unsigned long timestamp, long value)
 	}
 	
 	if (_tagtxpacket.tagcount == 0) {
+		// initialise timestamp
 		_tagtxpacket.timestamp = timestamp;
 
 		// set timeout value for the transmission delay
@@ -108,6 +164,7 @@ void TagExchange::publishLong(int tagid, unsigned long timestamp, long value)
 // this functions results in immediate publishing of the tags enqueued. returns the number of tag values send.
 // if the force parameter is set to true the function will block until the packet can be send, otherwise the 
 // function will give up if the RF12M modem is busy.
+#if 0
 int TagExchange::publishNow(bool force)
 {            
 	int res = 0;
@@ -132,7 +189,7 @@ int TagExchange::publishNow(bool force)
 //	}
 	return res;
 }
-
+#endif
 
 
 
@@ -151,65 +208,16 @@ TagExchangeRF12::TagExchangeRF12(void) : TagExchange()
 
 void TagExchangeRF12::poll(void) 
 {
-
     if (rf12_recvDone() && rf12_crc == 0) {
-        
-		if (rf12_len >= 1) {
-			switch(rf12_data[0]==1) {
-				case msg_tag: {
-					// process tag message
-					Packet_TagData *packet;
-				   
-					 packet = (Packet_TagData*)rf12_data;
-
-					 for (byte i = 0; i < packet->tagcount; ++i) {
-					  
-					   int  tagid = packet->data[i].tagid & 0x3fff;              // lower 14 bits determine the tagid;
-					   TagType tagtype = (TagType)((packet->data[i].tagid & 0xc000) >> 14);   // upper 2 bits determine the data type
-													   
-					   switch(tagtype) {              
-						 case tagtype_float:
-						   if (_floatHandler_callback != NULL) {
-								(*_floatHandler_callback)(tagid, packet->timestamp, packet->data[i].value.float_value);
-							}                   
-						   break;
-							 
-						 case tagtype_long:
-						   if (_longHandler_callback != NULL) {
-								(*_longHandler_callback)(tagid, packet->timestamp, packet->data[i].value.long_value);
-							}                 
-						   break;
-						   
-					   }               
-					}
-				}
-				break;
-				
-				
-				case msg_tagrequest:
-					Serial.println(F("Unhandled message: msg_tagrequest"));
-					break;
-					
-				case msg_taggroup:
-					Serial.println(F("Unhandled message: msg_taggroup"));
-					break;
-
-				case msg_taggrouprequest:
-					Serial.println(F("Unhandled message: msg_taggrouprequest"));
-					break;
-					
-				default:
-					Serial.println(F("Unhandled message: user defined"));
-			}
+		if (rf12_len >= 1) {		
+			processInboundPacket((Packet *)rf12_data);
         }
     }
-	
-	
+		
 	// send queued outgoing packets when the timeout value is reached
 	if ((_tagtxpacket.tagcount > 0 && millis() >= _tagtxpacket_timeout) | (_tagtxpacket.tagcount >= MAX_TAGS_PER_RF12_MSG)) {
 		publishNow(false);
 	}
-	
 }
 
 		
@@ -246,14 +254,229 @@ int TagExchangeRF12::publishNow(bool force)
 TagExchangeStream::TagExchangeStream(Stream* s) : TagExchange()
 {
 	_stream = s;
+	
+	
+	// initialise serial reception 
+	rx_state = idle;
+	rx_len = 0;
+	rx_bytes = 0;
+	
+	
 
 //	Serial.println("TagExchangeStream::TagExchangeStream()");
 }
 
 
+
+/*
+                         11111111112222222222333333333344444444445555555
+              00123456789012345678901234567890123456789012345678901234560000000
+   Test data:   $PKT:#12,1,2,0,0,0,0,A,0,B2,3,D6,4B,B,40,64,7,AC,1,X346\  
+                V    V   V                                         V   V
+	idle	====|    |	 |                                         |   |====
+	header		=====|   |                                         |   |
+	count		     ====|                                         |   |
+	payload		         ==========================================|   |
+	checksum	                                                   ====|
+	end  --> idle		                                                            
+	
+*/
+	
+	
+	
+bool TagExchangeStream::isHexNumber(char c)
+{
+	return ((c>='0' && c<='9') || (c>='A' && c<='F') || (c>='a' && c<='f'));
+}
+
+void TagExchangeStream::parseHexNumber(char c)
+{
+	c=toupper(c);
+	
+	if (c>='0' && c<='9') {
+		rx_value = (rx_value * 16) + (c-'0');
+	} else 
+	if (c>='A' && c<='F') {
+		rx_value = (rx_value * 16) + 10 + (c-'A');		
+	} else {
+		rx_value = 0;
+	}
+}	
+	
+
+// handle data coming in over serial stream
+bool TagExchangeStream::handleInput(char c)
+{
+	bool ret = false;
+	rx_len++;
+
+	switch (rx_state) {
+		case idle:
+			if (c=='$') {
+				// start of our packet
+				rx_len = 1;
+				rx_bytes = 0;			
+				rx_bytes_expected = 0;
+				rx_checksum = 0;
+				rx_value = 0;
+				
+				rx_state = header;				
+				ret = true;
+
+				//Serial.println(F("Input idle->header"));
+			} else {
+				rx_state = idle;
+				ret = false;
+			}
+			break;
+		
+		case header:			
+			if ((rx_len==2 && c=='P')
+			  ||(rx_len==3 && c=='K')
+			  ||(rx_len==4 && c=='T')
+			  ||(rx_len==5 && c==':')) {
+					rx_state = header;				
+					ret = true;
+					
+					//Serial.print(F("Input in header len="));
+					//Serial.println(rx_len);
+			} else
+			if (rx_len==6 && c=='#') {
+				rx_state = count;
+				ret = true;
+				
+				//Serial.println(F("Input header->count"));				
+			} else {
+				rx_state = idle;
+				ret = false;
+				
+				//Serial.println(F("Input unexpected; header->idle"));
+			}
+			break;
+		
+		case count:
+			if (isHexNumber(c)) {
+				parseHexNumber(c);
+				ret = true;
+			} else 
+			if (c==',') {
+				// end if count detected; move on to the payload
+				rx_bytes_expected = rx_value;
+				rx_value = 0;	
+				rx_state = payload;
+				ret = true;
+				
+				//Serial.println(F("Input count->payload"));	
+				//Serial.print(F("expected bytes = "));	
+				//Serial.println(rx_bytes_expected);	
+				
+			} else {
+				rx_state = idle;
+				ret = false;
+				
+				//Serial.println(F("Input unexpected; count->idle"));	
+			}
+		
+			break;
+
+		case payload:
+			if (isHexNumber(c)) {
+				parseHexNumber(c);
+				ret = true;
+			} else 
+			if (c==',') {
+				// complete value for payload packet detected; process and move on next
+				//rx_bytes_expected = rx_value;
+				
+				if (rx_bytes >= 0 && rx_bytes < MAX_SERIAL_BYTES && rx_bytes < rx_bytes_expected) {
+
+					rx_buffer[rx_bytes] = rx_value;
+					rx_checksum += rx_value;
+
+				//	Serial.print(F("Payload byte received = "));	
+				//	Serial.println(rx_value);	
+				//	Serial.print(F("        buffer index  = "));	
+				//	Serial.println(rx_bytes);	
+				//	Serial.print(F("        calc checksum = "));	
+				//	Serial.println(rx_checksum);	
+									
+					rx_value = 0;	
+					rx_bytes++;
+					ret = true;
+				} else {
+					// buffer overflow --> reject and go to idle state
+					
+					//Serial.print(F("TagExchangeStream::handleInput>> Incorrect payload length. rx_bytes="));
+					//Serial.print(rx_bytes);
+					//Serial.print(F(" expected="));
+					//Serial.println(rx_bytes);
+										
+					rx_state = idle;
+					ret = true;
+				}
+			} else 
+			if (c=='X') {
+				// start of checksum 
+								
+				rx_state = checksum;
+				rx_value = 0;	
+				ret = true;
+				
+				//Serial.println(F("Input payload"));	
+				//Serial.print(F("expected bytes = "));	
+				//Serial.println(rx_bytes_expected);					
+				//Serial.print(F("received bytes = "));	
+				//Serial.println(rx_bytes);	
+				
+			} else {
+				rx_state = idle;
+				ret = false;
+				
+				//Serial.println(F("Input unexpected; payload->idle"));	
+			}		
+			break;
+		
+		case checksum:
+			if (isHexNumber(c)) {
+				parseHexNumber(c);
+				ret = true;
+			} else 
+			if (c == '\\') {
+				// end of checksum
+
+				//Serial.println(F("Input checksum"));	
+				//Serial.print(F("received checksum   = "));	
+				//Serial.println(rx_value);	
+				//Serial.print(F("calculated checksum = "));	
+				//Serial.println(rx_checksum);	
+
+				if (rx_bytes == rx_bytes_expected && rx_value == rx_checksum) {
+					// received payload matches header structure, number of bytes and checksum
+				
+					//Serial.println(F("invoking processInboundPacket()"));	
+					_stream->println("OK");
+					processInboundPacket((Packet *)&rx_buffer);
+				}
+				rx_state = idle;
+				ret = true;
+			} else {
+				rx_state = idle;
+				ret = false;
+				
+				//Serial.println(F("Input unexpected; checksum->idle"));	
+			}
+			
+			break;
+	}
+	
+	return ret;
+}
+
+
+
 void TagExchangeStream::poll(void) 
 {
-	// FIXME: handle incoming data
+	// incoming data is handled via separate handleinput function
 	
 	
 	// send queued outgoing packets when the timeout value is reached
@@ -272,9 +495,17 @@ int TagExchangeStream::publishNow(bool force)
 	int res = 0;
 	
 	// send data to the stream object
-	_stream->print("$PKT:");
+	_stream->print("$PKT");
 	
 	byte txsize = 6 + (_tagtxpacket.tagcount * sizeof(TagData));
+	
+	// #bytes in packet
+	_stream->print(":#");	
+	_stream->print(txsize,HEX);
+	_stream->print(",");	
+	
+	
+	// data packet
 	byte* data = (byte*)(&_tagtxpacket);
 	unsigned int csum = 0;
 	
@@ -287,11 +518,10 @@ int TagExchangeStream::publishNow(bool force)
 	
 	// add checksum code to detect if packet was received correctly
 	// simple calculate a unsigned 16bit sum of all bytes in the data packet
-	_stream->print("X:");	
+	_stream->print("X");	
 	_stream->print(csum,HEX);
+	_stream->println("\\");	
 		
-	_stream->println(":END$");
-	
 	  
 #ifdef TAGXCH_DEBUG_TXBUFFER		  
 		Serial.println(F("DBG: TRANSMIT BUFFER"));
@@ -309,69 +539,69 @@ int TagExchangeStream::publishNow(bool force)
 
 
 // constructor
-TagExchangeHardwareSerial::TagExchangeHardwareSerial(HardwareSerial* s) : TagExchangeStream(s)
-{
-	_stream = s;
-
-
+//TagExchangeHardwareSerial::TagExchangeHardwareSerial(HardwareSerial* s) : TagExchangeStream(s)
+//{
+//	_stream = s;
+//
+//
 //	Serial.println("TagExchangeHardwareSerial::TagExchangeHardwareSerial()");
-	
-	
-}
-
-
-void TagExchangeHardwareSerial::poll(void) 
-{
-	// FIXME: handle incoming data
-	
-	
-	// send queued outgoing packets when the timeout value is reached
-	if ((_tagtxpacket.tagcount > 0 && millis() >= _tagtxpacket_timeout) | (_tagtxpacket.tagcount >= MAX_TAGS_PER_RF12_MSG)) {
-		publishNow(false);
-	}
-}
+//	
+//	
+//}
+//
+//
+//void TagExchangeHardwareSerial::poll(void) 
+//{
+//	// FIXME: handle incoming data
+//	
+//	
+//	// send queued outgoing packets when the timeout value is reached
+//	if ((_tagtxpacket.tagcount > 0 && millis() >= _tagtxpacket_timeout) | (_tagtxpacket.tagcount >= MAX_TAGS_PER_RF12_MSG)) {
+//		publishNow(false);
+//	}
+//}
 
 
 // this functions results in immediate publishing of the tags enqueued. returns the number of tag values send.
 // if the force parameter is set to true the function will block until the packet can be send, otherwise the 
 // function will give up if the RF12M modem is busy.
-int TagExchangeHardwareSerial::publishNow(bool force)
-{            
-	int res = 0;
-	
-	// send data to the stream object
-	
-	_stream->print(">>@HW-SERIAL:");
-	_stream->print("$PKT:");
-	
-	byte txsize = 6 + (_tagtxpacket.tagcount * sizeof(TagData));
-	byte* data = (byte*)(&_tagtxpacket);
-	unsigned int csum = 0;
-	
-	for (byte i = 0; i < txsize; ++i) {
-		byte b = *data++;
-		_stream->print(b,HEX);
-		_stream->print(",");	
-		csum += b;
-	}
-	
-	// add checksum code to detect if packet was received correctly
-	// simple calculate a unsigned 16bit sum of all bytes in the data packet
-	_stream->print("X:");	
-	_stream->print(csum,HEX);
-		
-	_stream->println(":END$");
-	
-	  
-#ifdef TAGXCH_DEBUG_TXBUFFER		  
-		Serial.println(F("DBG: TRANSMIT BUFFER"));
-#endif
-
-		res = _tagtxpacket.tagcount;
-	  
-		// init transmit buffer after sending
-	initTagTxBuffer();
-
-	return res;
-}
+//int TagExchangeHardwareSerial::publishNow(bool force)
+//{            
+//	int res = 0;
+//	
+//	// send data to the stream object
+//	
+//	_stream->print(">>@HW-SERIAL:");
+//	_stream->print("$PKT:");
+//	
+//	byte txsize = 6 + (_tagtxpacket.tagcount * sizeof(TagData));
+//	byte* data = (byte*)(&_tagtxpacket);
+//	unsigned int csum = 0;
+//	
+//	for (byte i = 0; i < txsize; ++i) {
+//		byte b = *data++;
+//		_stream->print(b,HEX);
+//		_stream->print(",");	
+//		csum += b;
+//	}
+//	
+//	// add checksum code to detect if packet was received correctly
+//	// simple calculate a unsigned 16bit sum of all bytes in the data packet
+//	_stream->print("X:");	
+//	_stream->print(csum,HEX);
+//		
+//	_stream->println(":END$");
+//	
+//	  
+//#ifdef TAGXCH_DEBUG_TXBUFFER		  
+//		Serial.println(F("DBG: TRANSMIT BUFFER"));
+//#endif
+//
+//		res = _tagtxpacket.tagcount;
+//	  
+//		// init transmit buffer after sending
+//	initTagTxBuffer();
+//
+//	return res;
+//}
 
--- TagExchange.h
+++ TagExchange.h
@@ -8,6 +8,10 @@
 // maximum number of tags that can be stored in a packet transmitted over the RF12 network
 #define MAX_TAGS_PER_RF12_MSG	10
 
+
+// maximum number of bytes to handle by serial reception buffer
+#define MAX_SERIAL_BYTES		80
+
 // maximum transission delay; defines how many ms the system will wait for additional tags 
 // to group in the same packet.
 #define MAX_TRANSMIT_DELAY		200
@@ -47,14 +51,54 @@ class TagExchange {
 				char  text_value[4];	// type 2 - tagtype_text
 			} value; 
 		} TagData;
-	
 
+
+		typedef struct {
+			byte tagcount;     // number of tags in packet
+			unsigned long timestamp;	// seconds since 1970; or 0 if unset.
+										// typical use is real-time messaging between nodes
+										// in this case the timestamp is not set.
+										// in case message will be buffered for extended amount of time
+										// the timestamp should be used to keep track of time of the messages
+  
+			// payload packet max size = 66 bytes
+			// 2+4=6 bytes for the header; leaves 60 bytes for the tag data
+			// 6 bytes per message: maximum 10 tags 
+			TagData data[MAX_TAGS_PER_RF12_MSG];		// FIXME -- max number of messages should be dynamic to allow different packet sizes 
+														// for serial/ethernet?
+														// 2nd thought... maybe not; 2nd alternative is to pack multiple of these packets in
+														// a single ethernet packet. MTU can be different in function of AVR memory (328p vs. mega)
+														// >> can we convert to a malloc'ed array; where is size defined in class invocation?														
+		} TagDataMsg;
+
+		
+		typedef struct {
+			byte data[5 + (6*MAX_TAGS_PER_RF12_MSG)];
+		} GenericMsg;
+		
+		
+		typedef struct {
+			// header
+			byte msg_id;      // identifier 
+			
+			union {
+				GenericMsg	Generic;
+				TagDataMsg	TagData;
+			} payload;
+		} Packet;
+	
+	
+	
 		typedef struct {
 			// header
 			byte msg_id;      // identifier 
   
 			byte tagcount;     // number of tags in packet
 			unsigned long timestamp;	// seconds since 1970; or 0 if unset.
+										// typical use is real-time messaging between nodes
+										// in this case the timestamp is not set.
+										// in case message will be buffered for extended amount of time
+										// the timestamp should be used to keep track of time of the messages
   
 			// payload packet max size = 66 bytes
 			// 2+4=6 bytes for the header; leaves 60 bytes for the tag data
@@ -66,6 +110,11 @@ class TagExchange {
 														// >> can we convert to a malloc'ed array; where is size defined in class invocation?														
 		} Packet_TagData;
 
+//		union {
+//		    Packet_Header		Header;	     // identifier 
+//			Packet_TagData		TagData;
+//		} Packet_t;
+		
 		
 		// constructor
 		TagExchange();
@@ -81,7 +130,7 @@ class TagExchange {
 		void publishLong(int tagid, long value);
 		void publishLong(int tagid, unsigned long timestamp, long value);
 		
-		int publishNow(bool force=true);	// returns the number of tags transmitted
+		virtual int publishNow(bool force=true) =0;	// returns the number of tags transmitted
 	
 
 	protected:
@@ -91,7 +140,8 @@ class TagExchange {
 		TagUpdateLongCallbackFunction	_longHandler_callback;
 		TagUpdateTextCallbackFunction	_textHandler_callback;
 		
-		void initTagTxBuffer(void);
+		void initTagTxBuffer(void);	
+		void processInboundPacket(Packet *packet);	// handle inbound message
 	
 
 		Packet_TagData _tagtxpacket;			// buffer for transmitting tag updates
@@ -124,35 +174,66 @@ class TagExchangeStream : public TagExchange
 		// constructor 
 		// mandatory argument is the Serial port we want to use
 		TagExchangeStream(Stream* s);
-		//
 		
 		void poll(void);
 		
 		int publishNow(bool force=true);	// returns the number of tags transmitted
 
+		bool handleInput(char c);			// handle data coming in over serial stream
+											// returns true of the input was handled, ie. it was recognised as valid serialised packet
 		
 	protected:
+	
+		bool isHexNumber(char c);	// return if c is any of "01234567890abcdefABCDEF"
+		void parseHexNumber(char c);		// parse hexadecimal number from input stream
+	
+	
 		Stream* _stream;
 		
-};
-
-
-
-class TagExchangeHardwareSerial : public TagExchangeStream
-{
-	public:
-		// constructor 
-		// mandatory argument is the Serial port we want to use
-		TagExchangeHardwareSerial(HardwareSerial* s);
 		
-		void poll(void);
+		// 
 		
-		int publishNow(bool force=true);	// returns the number of tags transmitted
+		typedef enum {
+			idle		= 1,
+			header		= 2,
+			count		= 3,
+			payload		= 4,
+			checksum	= 5
+		} RxState;
+		
+		
+		
+		byte rx_buffer[MAX_SERIAL_BYTES];
+		RxState  rx_state;			// state for input parser
+		unsigned int rx_value;				
+		int  rx_len;				// position in stream (0=not in stream; 1=1st character etc.
+		int  rx_bytes;				// number of bytes received
+		int  rx_bytes_expected;		// number of bytes expected (according to length indicator in
+		unsigned int rx_checksum;	// checksum
+		
+	
+	
+		};
 
-		
-	protected:
-		HardwareSerial* _stream;		
-};
+
+
+// not needed as we can use the Stream class for serial communications
+//
+//class TagExchangeHardwareSerial : public TagExchangeStream
+//{
+//	public:
+//		// constructor 
+//		// mandatory argument is the Serial port we want to use
+//		TagExchangeHardwareSerial(HardwareSerial* s);
+//		
+//		void poll(void);
+//		
+//		int publishNow(bool force=true);	// returns the number of tags transmitted
+//
+//		
+//	protected:
+//		HardwareSerial* _stream;		
+//};
 
 
 
